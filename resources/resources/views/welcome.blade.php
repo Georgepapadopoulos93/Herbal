418c643cedb225f0ecae16797493c5f35daf9930
Georgepapadopoulos93 <126574760+Georgepapadopoulos93@users.noreply.github.com>
2023-03-01 14:39:02 +0000

    Add files via upload

---
+++ resources/resources/views/welcome.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('asset/style.css') }}">
+    <link rel="stylesheet" href="style.css">
+    <title>Herbal - Fabyouless</title>
+    
+</head>
+
+
+
+
+
+<body id="page-107473" class="">
+
+    <div class="container page-container"> 
+        <div class="activation code"> 
+            <div class="wrapper">   
+                <h1 style="text-align: center" class="custom_heading">Book Your Free Beauty Treatment</h1>
+
+            </div>
+        <form id="vcglf_coupon_activation_code" novalidate="novalidate">
+            
+            </form>
+            <style>{
+                padding-left: 20px;
+                padding right: 20px;
+                width: auto;
+            
+            }
+            </style>
+        </div>
+    </div>
+        <h1>hello</h1>
+    <header>
+        Header
+    </header>
+    <main id="main" class="site-main">
+        <div class="wrapper">
+            <h1 style="text-align: center " class="custom_heading">Step 1: Find a participating salon near you</h1>
+        </div>
+            <h1 style="text-align: center" class="custom_heading">Step 2: Add your acticvation cocde</h1>
+        <div class="activation_container">
+            <div class="wrapper">
+                <form id="vcglf_actication_code" novalidate="novalidate">
+                    <label for ="vcglf_activation_code">Activation Code </label>
+                    <input type="text" class="form-control no-label"autocomplete="off" name="vcglf_activation_code" id="activation_code"placeholder value>
+                    <input type="hidden" id="activation_code_coupon_id"value="107473">
+                    <input type="hidden" id ="activation_codeproduct_id"value>
+                </form>
+            </div>
+            <h1 style="text-align: center" class="custom_heading">Step : Appointment Request Form</h1>
+            <div class="sgv-product-add-to-cart-container text-left">
+                <form class="sgv-product-add-to-cart" action="" method="post" autocomplete="off">
+                    <p> 
+                        <label for="product_add_to_cart_postal_code">
+                            Your postcode<span class="required">*</span>
+                        </label>
+                        <input id="product_add_to_cart_postal_code" class="form-control sgv-day-picker" type="text" name="product_add_to_cart_postal_code" />
+                    </p>
+                    <p>
+                        <label for="product_add_to_cart_salon">
+                            Preferred salon<span class="required">*</span>
+                        </label>
+                        <input id="product_add_to_cart_salon" class="form-control" type="text" name="product_add_to_cart_salon"  />
+                    </p>
+                    <p>
+                        <label for="product_add_to_cart_treatments">
+                            Treatment<span class="required">*</span>
+                        </label>
+                            <select id="product_add_to_cart_treatments" class="treatments form-control" name="product_add_to_cart_treatments">
+                                <option value="">Please select</option>
+                                    <option value="blow-dry">Blow Dry</option>
+                                    <option value="manicure">Manicure</option>
+                                    <option value="pedicure">Pedicure</option>
+                                    <option value="back-neck-shoulder-massage">Back Neck and Shoulder Massage</option>
+                                    <option value="facial">Facial</option>
+                            </select>
+                    <p>
+                        Please selected 3 preferred dates. These must be at least 2 weeks from today and at least 2 weeks apart but no later than 30/9/23.        
+                    </p>
+                    <p>
+                        <label for="product_add_to_cart_day_1">
+                            Day 1<span class="required">*</span>
+                            </label>
+                                <input id="product_add_to_cart_day_1" class="form-control sgv-day-picker" type="text" name="product_add_to_cart_day_1"  />
+                                <span id="product_add_to_cart_day_1_clear" class="input-clear-button">Clear</span>
+                    </p>
+                    <p>
+                        <label for="product_add_to_cart_day_2">
+                            Day 2<span class="required">*</span>
+                        </label>
+                            <input id="product_add_to_cart_day_2" class="form-control sgv-day-picker" type="text" name="product_add_to_cart_day_2" disabled  />
+                            <span id="product_add_to_cart_day_2_clear" class="input-clear-button">Clear</span>
+                    </p>
+                    <p>
+                        <label for="product_add_to_cart_day_3">
+                            Day 3<span class="required">*</span>
+                        </label>
+                        <input id="product_add_to_cart_day_3" class="form-control sgv-day-picker" type="text" name="product_add_to_cart_day_3" disabled />
+                        <span id="product_add_to_cart_day_3_clear" class="input-clear-button">Clear</span>
+                    </p>
+                    <p class="submit-button">
+                        <input type="hidden" name="sgv-product-id" value="107472" />
+                        <input type="hidden" name="sgv-action-id" value="svg-product-add-to-cart" />
+                        <input type="hidden" id="sgv-product-add-to-cart-nonce" name="sgv-product-add-to-cart-nonce" value="a3a148c6d4" /><input type="hidden" name="_wp_http_referer" value="/herbal/" />            <button class="form-control btn btn-primary" type="submit">
+                            Continue                <span class="arrow">
+                        </span>
+                        </button>
+                    </p>
+                </form>
+                    
+            </div>
+        </div>
+    </main>
+    <footer class="product-meta-footer">
+                <div class="container">
+                    <div class="text-center">
+                        <div class="footer-powered-by">
+                            Powered by
+                            <a href="https://www.fabyouless.co.uk/" target="_blank">
+        						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1110 254" aria-hidden="true">
+        							<use xlink:href="https://www.fabyouless.co.uk/wp-content/themes/subscription-group-fabyouless/map.svg#svg-fabyouless-logo-large"></use>
+        						</svg>
+        						<span class="sr-only">fabyouless</span>
+        					</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+</body>
+</html>
