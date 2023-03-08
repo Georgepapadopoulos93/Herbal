69184df228f5f7a15021698ef279c4daee61007b
Georgepapadopoulos93 <126574760+Georgepapadopoulos93@users.noreply.github.com>
2023-03-08 10:55:13 +0000

    Add files via upload

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,45 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-   
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/script.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCojgiav8kJxwSHGggpIGgKFw-8ywXVa5Q&callback=initMap" async defer></script>
 
     <script>
-var map;
-var geocoder;
+        var map;
+        var geocoder;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.7749, lng: -122.4194},
-    zoom: 8
-  });
-  geocoder = new google.maps.Geocoder();
-}
+        function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 37.7749, lng: -122.4194},
+            zoom: 8
+        });
+        geocoder = new google.maps.Geocoder();
+        }
 
-function search() {
-  var address = document.getElementById('address').value;
-  var city = document.getElementById('city').value;
-  var state = document.getElementById('state').value;
-  var zip = document.getElementById('zip').value;
-  
-  var fullAddress = address + ', ' + city + ', ' + state + ' ' + zip;
-  
-  geocoder.geocode({'address': fullAddress}, function(results, status) {
-    if (status === 'OK') {
-      var location = results[0].geometry.location;
-      map.setCenter(location);
-      var marker = new google.maps.Marker({
-        map: map,
-        position: location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
-</script>
+        function search() {
+        var address = document.getElementById('address').value;
+        var city = document.getElementById('city').value;
+        var state = document.getElementById('state').value;
+        var zip = document.getElementById('zip').value;
+        
+        var fullAddress = address + ', ' + city + ', ' + state + ' ' + zip;
+        
+        geocoder.geocode({'address': fullAddress}, function(results, status) {
+            if (status === 'OK') {
+            var location = results[0].geometry.location;
+            map.setCenter(location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: location
+            });
+            } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+        }
+    </script>
     <title>Herbal - Fabyouless</title>
 </head>
 
